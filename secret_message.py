2457a2eead4c91d77f2da87a921c573e7c409b65
Nazanin <n.zinouri@gmail.com>
2017-12-17 14:23:10 -0500

    get a list of names of all files in a folder

---
+++ secret_message.py
@@ -0,0 +1,10 @@
+import os
+
+def rename_files():
+    #step1: get file names from a folder
+    file_list = os.listdir(r"C:\Users\nazanin\Desktop\HHIV\Intro to Programming Nanodegree\4-Create a Movie Website\Lesson2_mini_project_secret_message\prank\prank")
+    print file_list
+
+    #step2: for each file, rename files
+
+rename_files()
