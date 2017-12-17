ada1ec5c0fdee2fd769cd90d60a54560cff2353c
Nazanin <n.zinouri@gmail.com>
2017-12-17 18:29:33 -0500

    personalizing the message

--- secret_message.py
+++ secret_message.py
@@ -2,11 +2,11 @@ import os
 
 def rename_files():
     #step1: get file names from a folder
-    file_list = os.listdir(r"C:\Users\nazanin\Desktop\HHIV\Intro to Programming Nanodegree\4-Create a Movie Website\Lesson2_mini_project_secret_message\prank\prank")
+    file_list = os.listdir(r"C:\Users\nazanin\Desktop\HHIV\Intro to Programming Nanodegree\4-Create a Movie Website\Lesson2_mini_project_secret_message\alphabet2")
     print file_list
     saved_path = os.getcwd()
     print saved_path
-    os.chdir(r"C:\Users\nazanin\Desktop\HHIV\Intro to Programming Nanodegree\4-Create a Movie Website\Lesson2_mini_project_secret_message\prank\prank")
+    os.chdir(r"C:\Users\nazanin\Desktop\HHIV\Intro to Programming Nanodegree\4-Create a Movie Website\Lesson2_mini_project_secret_message\alphabet2")
 
     #step2: for each file, rename files
     for file_name in file_list:
