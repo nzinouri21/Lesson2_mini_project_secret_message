119c461f84ad968d83a37986b65713c39a73f8c4
Nazanin <n.zinouri@gmail.com>
2017-12-17 17:40:06 -0500

    end

--- secret_message.py
+++ secret_message.py
@@ -4,7 +4,15 @@ def rename_files():
     #step1: get file names from a folder
     file_list = os.listdir(r"C:\Users\nazanin\Desktop\HHIV\Intro to Programming Nanodegree\4-Create a Movie Website\Lesson2_mini_project_secret_message\prank\prank")
     print file_list
+    saved_path = os.getcwd()
+    print saved_path
+    os.chdir(r"C:\Users\nazanin\Desktop\HHIV\Intro to Programming Nanodegree\4-Create a Movie Website\Lesson2_mini_project_secret_message\prank\prank")
 
     #step2: for each file, rename files
+    for file_name in file_list:
+        print "Old name " + file_name
+        os.rename(file_name,file_name.translate(None, "0123456789"))
+        print "New name " + file_name.translate(None, "0123456789")
+
 
 rename_files()
